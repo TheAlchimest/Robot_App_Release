a6b20f7f65f3b0a4fa452fa7d0f8f3502a0f8b04
TheAlchimest5 <TheAlchimest5@gmail.com>
2025-10-30 00:59:46 +0300

    updates

--- .cmd
+++ .cmd
@@ -11,5 +11,18 @@ python main.py --allow_wake_word --device=windows --eye_model=video
 python main.py --allow_wake_word=False --device=windows --eye_model=video
 
 
+### raspberrypi
+pi@raspberrypi:~/apps/Robot_App_Release $ git clone https://github.com/TheAlchimest/Robot_App_Release.git
+
+pi@raspberrypi:~ $ cd /home/pi/apps/Robot_App_Release
+pi@raspberrypi:~/apps/Robot_App_Release $ python -m venv .venv
+pi@raspberrypi:~/apps/Robot_App_Release $ pip install -r requirements.txt
+
+pi@raspberrypi:~/apps/Robot_App_Release $ python main.py --allow_wake_word=False --device=raspi5 --eye_model=none
+pi@raspberrypi:~/apps/Robot_App_Release $ git pull
+
+
+
+
 
 
